f82db375938dd44adc1d8999b06566771dea0ebb
Mike Feldberg <mikefeldberg@gmail.com>
2019-06-14 14:56:50 -0700

    add signup and login templates and functionality; add base template w header

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
